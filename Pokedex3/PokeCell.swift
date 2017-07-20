37614cb8d79e5166bbf68c2158f74d8572da7979
Brian Marquis <rbmarquis.dev@gmail.com>
2017-07-19 21:02:13 -0500

    Created PokeCell class for UIColletionViewCell, connected to storyboard.

---
+++ Pokedex3/PokeCell.swift
@@ -0,0 +1,33 @@
+//
+//  PokeCell.swift
+//  Pokedex3
+//
+//  Created by Brian Marquis on 7/19/17.
+//  Copyright © 2017 brimarq. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    // Create rounded corners on cells
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // Each view has a layer level that can be used to modify its display
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+}
