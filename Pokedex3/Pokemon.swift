fccb0745b959a383c1d7d1a021692e2cd01a0500
Brian Marquis <rbmarquis.dev@gmail.com>
2017-07-25 16:57:59 -0500

    Finished api download code for update of bio segment of detail VC

--- Pokedex3/Pokemon.swift
+++ Pokedex3/Pokemon.swift
@@ -22,9 +22,33 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _nextEvolutionTxt: String!
+    private var _nextEvolutionName: String!
+    private var _nextEvolutionID: String!
+    private var _nextEvolutionLevel: String!
     private var _pokemonURL: String!
     
     // getters
+    
+    var nextEvolutionName: String {
+        if _nextEvolutionName == nil {
+            _nextEvolutionName = ""
+        }
+        return _nextEvolutionName
+    }
+    
+    var nextEvolutionID: String {
+        if _nextEvolutionID == nil {
+            _nextEvolutionID = ""
+        }
+        return _nextEvolutionID
+    }
+    
+    var nextEvolutionLevel: String {
+        if _nextEvolutionLevel == nil {
+            _nextEvolutionLevel = ""
+        }
+        return _nextEvolutionLevel
+    }
     
     var description: String {
         if _description == nil {
@@ -143,6 +167,72 @@ class Pokemon {
                     
                 } else {
                     self._type = ""
+                }
+                
+                // Parse JSON data for the description field.
+                if let descArr = dict["descriptions"] as? [Dictionary<String, String>] , descArr.count > 0 {
+                    
+                    if let url = descArr[0]["resource_uri"] {
+                        
+                        let descURL = "\(URL_BASE)\(url)"
+                        
+                        Alamofire.request(descURL).responseJSON(completionHandler: { (response) in
+                            
+                            if let descDict = response.result.value as? Dictionary<String, AnyObject> {
+                                
+                                if let description = descDict["description"] as? String {
+                                    
+                                    // Correct spelling in original JSON data.
+                                    let newDescription = description.replacingOccurrences(of: "POKMON", with: "Pokemon")
+                                    
+                                    self._description = newDescription
+                                    
+                                    print(newDescription)
+                                }
+                            }
+                            completed()
+                        })
+                    }
+                } else {
+                    
+                    // In case no description data exists, protect from nil.
+                    self._description = ""
+                }
+                
+                // Parse JSON data for the description field.
+                if let evolutions = dict["evolutions"] as? [Dictionary<String, AnyObject>] , evolutions.count > 0 {
+                    
+                    if let nextEvo = evolutions[0]["to"] as? String {
+                        
+                        if nextEvo.range(of: "mega") == nil {
+                            
+                            self._nextEvolutionName = nextEvo
+                            
+                            if let uri = evolutions[0]["resource_uri"] as? String {
+                                
+                                let newStr = uri.replacingOccurrences(of: "/api/v1/pokemon/", with: "")
+                                let nextEvoId = newStr.replacingOccurrences(of: "/", with: "")
+                                
+                                self._nextEvolutionID = nextEvoId
+                                
+                                if let lvlExist = evolutions[0]["level"] {
+                                    
+                                    if let lvl = lvlExist as? Int {
+                                        
+                                        self._nextEvolutionLevel = "\(lvl)"
+                                    }
+                                    
+                                    
+                                } else {
+                                    self._nextEvolutionLevel = ""
+                                }
+                            }
+                        }
+                    }
+                    
+                    print(self._nextEvolutionLevel)
+                    print(self._nextEvolutionName)
+                    print(self._nextEvolutionID)
                 }
 
             }
